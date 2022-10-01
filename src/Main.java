12e34ab2d7a7eeb9d28a46e0f9b498b3cfc9b0a5
Fabrício Vaz <fabricio.vaz2004@gmail.com>
2022-10-01 10:05:50 -0300

    Primeiro Commit dessa questão

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int num = 0;
+
+        System.out.print("Digite um numero: ");
+        num = sc.nextInt();
+
+        System.out.println(" O numero informado foi: " + num);
+    }
+}
